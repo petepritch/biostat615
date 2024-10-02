a771ec3a7b8e88ffa3d23ccde8325bae7ddc017f
petepritch <petepritchard16@gmail.com>
2024-10-02 12:43:40 -0400

    saved changes

---
+++ euclideanDistanceThreshol.R
@@ -0,0 +1,13 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title Educlidian Distance Threshold
+##' @param X
+##' @param Y
+##' @param thres
+##' @return Number of column pairs with Euclidian distance less than or equal to
+##'         thres from two matrices X and Y with the same number of rows.
+##' @author Pete Pritchard
+euclideanDistanceThreshol <- function(X, Y, thres) {
+
+}
